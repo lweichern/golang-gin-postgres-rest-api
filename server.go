c0558b87febd6731df00347eb4f5cf342b4e7816
Wei chern Lim <weichernlim@Weis-MacBook-Pro.local>
2024-01-29 23:27:45 +0800

    Change port and exposed url for railway deployment

--- server.go
+++ server.go
@@ -4,17 +4,24 @@ import (
 	"example/http-server/database"
 	"example/http-server/middleware"
 	"example/http-server/routes"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-
-func main(){
+func main() {
 	router := gin.Default()
 	database.ConnectDatabase()
 	routes.UserRoute(router)
 	router.Use(middleware.AuthMiddleware) // any route below this will be protected routes
 	routes.AuthorRoute(router)
 	routes.BookRoute(router)
-	router.Run("localhost:8080")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router.Run("0.0.0.0:" + port)
 }
